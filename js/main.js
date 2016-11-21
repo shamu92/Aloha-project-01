e249854188a9f0534f2966954564d279080f48aa
shamu92 <altshteyn@gmail.com>
2016-11-21 15:23:43 -0800

    created fixed header and fixed cart quantity count

--- js/main.js
+++ js/main.js
@@ -1,50 +1,42 @@
-$( document).ready(function(){
+$(document).ready(function() {
 
-// Flickty carousel
+    // Flickty carousel
 
-    $('.carousel').flickity({
+    // $('.carousel').flickity({
+    //     cellAlign: 'left',
+    //     contain: true,
+    //     autoPlay: true,
+    //     prevNextButtons: false // the arrrows still show up for some reason
+    // });
 
-        cellAlign: 'left',
-        contain: true,
-        autoPlay: true,
-		prevNextButtons: false // the arrrows still show up for some reason
-  });
-
-// smooth scrolling
-    $('a').click(function(){
+    // smooth scrolling
+    $('a').click(function() {
         $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
+            scrollTop: $($(this).attr('href')).offset().top
         }, 1250);
         return false;
+    });
+
+    // alert for email form
+
+    $(function() {
+        $('.email-sign-up').on('submit', 'form', function(event) {
+            event.preventDefault();
+            var $email = $('#your-email')
+
+            if ($email.val().length !== 0) {
+                alert('Thanks for subscribing!');
+                $email.val('');
+            } else {
+                alert('Please submit a valid email address.');
+            }
+        });
+
+    });
 });
+// This is code for the add item to cart feature. It doesn't work at the moment. need to fix actual cart
+$('.addCart').click(function() {
+    $('.simpleCart_quantity').fadeIn('fast').html(function(i, val) { return val * 1 + 1 });
+})
 
-// alert for email form
-
-     $(function() {
-		$('.email-sign-up').on('submit', 'form', function(event) {
-			event.preventDefault();
-			var $email = $('#your-email')
-
-			if ( $email.val().length !== 0 ) {
-				alert('Thanks for subscribing!');
-				$email.val('');
-			} else {
-				alert('Please submit a valid email address.');
-			}
-		});
-        
-	});
-});
-    // This is code for the add item to cart feature. It doesn't work at the moment. need to fix actual cart
-	/*simpleCart({
-			checkout: { 
-				type: "PayPal" , 
-				email: "you@yours.com", 
-			},
-            cartColumns: { 
-                attr: "quantity" , label: "Qty" },
-
-		});	
-    // simple Cart */
-    
-
+// simple Cart
